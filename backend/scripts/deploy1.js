ec5fe5f4761e8115379d4f169b9cfa1db3bc1cdb
H-strangeone <hiteshgitman@gmail.com>
2025-04-01 02:22:39 +0530

    ii

--- backend/scripts/deploy1.js
+++ backend/scripts/deploy1.js
@@ -14,3 +14,21 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+const { ethers } = require("hardhat");
+
+async function main() {
+  const Layer2 = await ethers.getContractFactory("Layer2");
+  const layer2 = await Layer2.deploy();
+  await layer2.deployed();
+  console.log(`Layer2 contract deployed at: ${layer2.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
